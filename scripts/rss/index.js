20d3dcdb192bfc65ab61c1297f9d749652659fae
canisminor1990 <canisminor1990@gmail.com>
2018-03-16 12:21:58 +0800

    sync

--- scripts/rss/index.js
+++ scripts/rss/index.js
@@ -1,30 +1,30 @@
 import RSS from 'rss';
 import { join } from 'path';
 import _ from 'lodash';
-import moment from 'moment'
-import marked from 'marked'
-import fs from 'fs-extra'
+import moment from 'moment';
+import marked from 'marked';
+import fs from 'fs-extra';
 
-const SITE     = 'https://canisminor.cc';
+const SITE     = 'canisminor.cc';
 const pathDist = './dist';
 marked.setOptions({
-	                  renderer: new marked.Renderer(),
-	                  gfm: true,
-	                  tables: true,
-	                  breaks: false,
-	                  pedantic: false,
-	                  sanitize: false,
-	                  smartLists: true,
-	                  smartypants: false,
+	                  renderer   : new marked.Renderer(),
+	                  gfm        : true,
+	                  tables     : true,
+	                  breaks     : false,
+	                  pedantic   : false,
+	                  sanitize   : false,
+	                  smartLists : true,
+	                  smartypants: false
                   });
 
-const feed     = new RSS(
+const feed = new RSS(
 	{
 		title         : 'CanisMinor RSS Feed',
 		description   : '来自 CanisMinor 博客的订阅信息 - CanisMinor RSS Feed',
-		site_url      : SITE,
-		feed_url      : join(SITE, 'rss'),
-		image_url     : join(SITE, 'img/share-cover.png'),
+		site_url      : 'https://' + SITE,
+		feed_url      : 'https://' + join(SITE, 'rss'),
+		image_url     : 'https://' + join(SITE, 'img/share-cover.png'),
 		managingEditor: 'CanisMinor',
 		webMaster     : 'CanisMinor',
 		copyright     : 'CanisMinor 2018 版权所有',
@@ -39,13 +39,13 @@ export default cb => {
 	_.forEach(Toc, item => {
 		feed.item(
 			{
-				title          : item.title,
-				description    : marked(item.body),
-				guid           : join(SITE, 'blog', item.filename),
-				url            : join(SITE, 'blog', item.filename),
-				author         : 'CanisMinor',
-				categories     : item.tag,
-				date           : moment(item.filename.split('_')[1],'YYYYMMDDhhmmss').format("MMMM Do YYYY, h:mm:ss a")
+				title      : item.title,
+				description: marked(item.body),
+				guid       : 'https://' + join(SITE, 'blog', item.filename),
+				url        : 'https://' + join(SITE, 'blog', item.filename),
+				author     : 'CanisMinor',
+				categories : item.tag,
+				date       : moment(item.filename.split('_')[1], 'YYYYMMDDhhmmss').format('MMMM Do YYYY, h:mm:ss a')
 			});
 	});
 
